f7e8889db90556a4477af66a944ec82cf7980984
NansBarthelemy <151943649+NansBarthelemy@users.noreply.github.com>
2024-04-15 15:15:18 +0200

    third commit

--- Script/Data_Analysis_Script.R
+++ Script/Data_Analysis_Script.R
@@ -8,6 +8,7 @@ install.packages("agricolae")
 install.packages("egg")
 install.packages("tidyverse")
 install.packages("viridis")
+install.packages("ggsignif")
 
 library(ggplot2)
 library(dplyr)
@@ -24,6 +25,7 @@ setwd(dir="C:/Users/Nans/Desktop/PVC_Fragmentation_Drying/Data")
 ################################## Hypothesis 1 : Abundance of formed MPs depending on the DD treatments
 
 Abundance<-read.csv("H1_abundance.csv", header = T, sep = ";")
+
 # Data representation
 
 UVC_abundance_plot<-ggplot(data=Abundance, aes(x=Treatment, y=MPs3, fill=Treatment))+
@@ -42,10 +44,12 @@ bartlett.test(MPs3~Treatment, data=Abundance)
 kruskal.test(MPs3~Treatment, data=Abundance)
 kruskal(Abundance$MPs3, Abundance$Treatment, group=TRUE)$groups 
 
-UVC_abundance_plot + geom_signif(
+UVC_abundance_plot <-UVC_abundance_plot + geom_signif(
   y_position = c(200, 220, 180, 190, 230, 550, 300, 90, 90), xmin = c(1,2,3,4,5,6,7,8,9), xmax = c(1,2,3,4,5,6,7,8,9),
   annotation = c("BC", "B","BC","C","A","A","A","D",'D'), tip_length = 0.001
 )
+
+UVC_abundance_plot 
 
 ################################## Hypothesis 2 : Mass loss of the PVC film depending on the DD treatments
 
@@ -70,10 +74,13 @@ bartlett.test(Fraction2~Treatment, data=Mass_loss)
 kruskal.test(Fraction2~Treatment, data=Mass_loss)
 kruskal(Mass_loss$Fraction2,Mass_loss$Treatment, group=TRUE)$groups
 
-UVC_Mass_loss_plot+ geom_signif(
+UVC_Mass_loss_plot <- UVC_Mass_loss_plot+ geom_signif(
   y_position = c(0.3, 0.58, 0.20, 0.88, 0.72, 1, 0.38, 0.18, 0.28), xmin = c(1,2,3,4,5,6,7,8,9), xmax = c(1,2,3,4,5,6,7,8,9),
   annotation = c("D", "B","C","A","A","A","B","C",'CD'), tip_length = 0.001
 )
+
+UVC_Mass_loss_plot
+
 ################################## Hypothesis 3 : Average size of the form MPs depending on the DD treatments
 
 Size<-read.csv("H3_size.csv", header = T, sep = ";")
@@ -97,11 +104,12 @@ bartlett.test(Size~Treatment, data=Size)
 kruskal.test(Size~Treatment, data = Size)
 kruskal(Size$Size, Size$Treatment, group=TRUE)$groups
 
-UVC_size_plot + geom_signif(
+UVC_size_plot <- UVC_size_plot + geom_signif(
   y_position = c(310, 530, 380, 540, 505, 580, 310, 220, 240), xmin = c(1,2,3,4,5,6,7,8,9), xmax = c(1,2,3,4,5,6,7,8,9),
   annotation = c("D", "B","C","AB","A","A","CD","E",'E'), tip_length = 0.001
 )
 
+UVC_size_plot 
 
 ################################## UV-C and Sunlight exposure comparison
 
@@ -130,6 +138,7 @@ SUN_abundance_plot+ geom_signif(
   annotation = c("E", "E","E","D","D","D","D","C",'C',"C","AB","ABC","BC","A"), tip_length = 0.001
 )
 
+SUN_abundance_plot
 
 # Data representation of the average size of formed MPs
 
@@ -148,10 +157,12 @@ bartlett.test(Size~Treatment, data=SUN)
 kruskal.test(Size~Treatment, data = SUN)
 kruskal(SUN$Size, SUN$Treatment, group=TRUE)$groups
 
-SUN_size_plot+ geom_signif(
+SUN_size_plot <- SUN_size_plot+ geom_signif(
   y_position = c(210, 230, 240, 185, 155, 180, 200, 215, 215, 215, 210, 245, 315, 300), xmin = c(1,2,3,4,5,6,7,8,9,10,11,12,13,14), xmax = c(1,2,3,4,5,6,7,8,9,10,11,12,13,14),
   annotation = c("EF", "DE","CD","EF","F","EF","DE","DE",'DEF',"CD","DE","BC","AB","A"), tip_length = 0.001
 )
+
+SUN_size_plot
 
 ################################## Graphical representation of the IBPFI depending on the models output
 
@@ -171,3 +182,8 @@ IBPFI_plot <- ggplot(IBPFIs, aes(x=" ", y=IBPFI))+
         axis.ticks.x=element_blank())
 IBPFI_plot
 
+Graph1<-plot_grid(UVC_abundance_plot,UVC_size_plot,UVC_Mass_loss_plot,ncol=3, nrow=1,labels = c("A","B","C"))
+Graph1
+
+
+ 
